febb01c6ce0918bb940bc0a76acee823827973aa
Anthony <quocanh1104@gmail.com>
2019-07-30 23:32:58 +0930

    run production

---
+++ build/precache-manifest.25a0ef37c288c20dc28a50a96210757e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b961bddcdaf3d7c3ed33912e23c999d7",
+    "url": "/Master-Chef-App/index.html"
+  },
+  {
+    "revision": "0c138cd819b4bca40a04",
+    "url": "/Master-Chef-App/static/css/2.cfa49cb9.chunk.css"
+  },
+  {
+    "revision": "bbee87be782eaaac3334",
+    "url": "/Master-Chef-App/static/css/main.94ba3a96.chunk.css"
+  },
+  {
+    "revision": "0c138cd819b4bca40a04",
+    "url": "/Master-Chef-App/static/js/2.bf01e044.chunk.js"
+  },
+  {
+    "revision": "bbee87be782eaaac3334",
+    "url": "/Master-Chef-App/static/js/main.7835cef8.chunk.js"
+  },
+  {
+    "revision": "85f178d263e820ac532b",
+    "url": "/Master-Chef-App/static/js/runtime~main.62b0e0e2.js"
+  },
+  {
+    "revision": "bb2f6ab31abfa73b9652859d3d01a6a0",
+    "url": "/Master-Chef-App/static/media/home-img.bb2f6ab3.jpg"
+  },
+  {
+    "revision": "f64c558634872d23c552fbdacef511a3",
+    "url": "/Master-Chef-App/static/media/logo.f64c5586.png"
+  }
+]);
