9866279acd95cd937d08db4e40158dc989b572db
Anthony <quocanh1104@gmail.com>
2019-07-31 01:26:35 +0930

    added homepage in package.json

---
+++ build/precache-manifest.ba44a2c861fa7e671d6a5928e6082043.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13c8dcd34b26e7499732ca383fea61e9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0c138cd819b4bca40a04",
+    "url": "/static/css/2.cfa49cb9.chunk.css"
+  },
+  {
+    "revision": "33fef43a56854ea2d630",
+    "url": "/static/css/main.94ba3a96.chunk.css"
+  },
+  {
+    "revision": "0c138cd819b4bca40a04",
+    "url": "/static/js/2.bf01e044.chunk.js"
+  },
+  {
+    "revision": "33fef43a56854ea2d630",
+    "url": "/static/js/main.1cf7e50f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "bb2f6ab31abfa73b9652859d3d01a6a0",
+    "url": "/static/media/home-img.bb2f6ab3.jpg"
+  },
+  {
+    "revision": "f64c558634872d23c552fbdacef511a3",
+    "url": "/static/media/logo.f64c5586.png"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Master-Chef-App/precache-manifest.25a0ef37c288c20dc28a50a96210757e.js"
+  "/precache-manifest.ba44a2c861fa7e671d6a5928e6082043.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Master-Chef-App/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
